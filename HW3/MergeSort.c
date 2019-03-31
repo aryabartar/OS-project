5a36fd6e9db37b1b2c0e4de88b8fa07661d67bef
arya <a.khaligh@aut.ac.ir>
2019-03-31 11:48:00 +0430

    Add merge

--- HW3/MergeSort.c
+++ HW3/MergeSort.c
@@ -8,6 +8,7 @@
 
 void fillData(int a[], int);
 void mergeSort (int *, int l , int r);
+void merge(int array[], int , int , int );
 
 int main()
 {
@@ -43,7 +44,8 @@ int main()
     // destroy the shared memory 
     shmctl(shmid,IPC_RMID,NULL); 
 
-    printf("%d\n" , shmid);
+
+
     return 0;
 }
 
@@ -95,5 +97,59 @@ void mergeSort (int *array, int l , int r)
             exit(0);
         }
     }
+
+	int status; 
+
+    // Wait for child processes to finish 
+	waitpid(lfork_status,0, 0); 
+	waitpid(rfork_status, 0, 0); 
     
+    merge(array , l , l+array_length/2 -1 , r);
 }
+
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    int L[n1], R[n2]; 
+  
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+    printf("Merge is eunning!\n");
+} 
