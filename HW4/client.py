0624dcdebd7fa29b142e316c313b6ba1a5ac674f
Arya Khaligh <bartararya@gmail.com>
2019-04-10 17:27:51 +0430

    Modify server code

--- HW4/client.py
+++ HW4/client.py
@@ -14,44 +14,16 @@ def start_connections(host, port, client_name):
     print("starting connection", client_name, "to", server_addr)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setblocking(False)
-    sock.connect(server_addr)
+    sock.connect_ex(server_addr)
     return sock
 
 
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-
-    if mask & selectors.EVENT_READ:
-        print("READ EVENT")
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            print("received", repr(recv_data),
-                  "from connection", data.client_name)
-            data.recv_total += len(recv_data)
-        if not recv_data or data.recv_total == data.message_len:
-            print("closing connection", data.client_name)
-            sel.unregister(sock)
-            sock.close()
-
-    if mask & selectors.EVENT_WRITE:
-        print("\nWRITE EVENT")
-        print(sock)
-        if not data.outb and data.message:
-            data.outb = message.encode()
-        if data.outb:
-            print("sending", repr(data.outb),
-                  "to connection", data.client_name)
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
-
-
 def write(sock):
-    pass
+    sock.send("hello".encode())
 
 
 def read(sock):
-    pass
+    print(sock.recv(1024))
 
 
 if len(sys.argv) != 4:
@@ -64,17 +36,18 @@ sock = start_connections(host, int(port), client_name)
 
 try:
     write_thread = threading.Thread(
-        target=write(), name="write_thread", args=(sock))
-    read_thread = threading.Thread(
-        target=read(), name="read_thread", args=(sock))
+        target=write, name="write_thread", args=(sock,))
+    # read_thread = threading.Thread(
+    #     target=read, name="read_thread", args=(sock,))
 
     write_thread.start()
-    read_thread.start()
+    # read_thread.start()
 
     write_thread.join()
-    read_thread.join()
+    # read_thread.join()
 
 except KeyboardInterrupt:
     print("caught keyboard interrupt, exiting")
 finally:
-    sel.close()
+    print("Closing connection!")
+    sock.close()
